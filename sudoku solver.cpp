67b896cf489b70af7a3aa987bb4f38a2ceccbf49
azizrosyid <34888698+azizrosyid@users.noreply.github.com>
2021-01-10 23:30:26 +0700

    add example sudoku unsolved

--- sudoku solver.cpp
+++ sudoku solver.cpp
@@ -39,6 +39,34 @@ int main() {
         cout << "No Solution For this Sudoku!" << endl;
     }
 
+    // easy sudoku
+    int sudokuUnsolved2[9][9] = {{5, 3, 0, 0, 7, 0, 0, 0, 0},
+                                 {6, 0, 0, 1, 9, 5, 0, 0, 0},
+                                 {0, 9, 8, 0, 0, 0, 0, 6, 0},
+                                 {8, 0, 0, 0, 6, 0, 0, 0, 3},
+                                 {4, 0, 0, 8, 0, 3, 0, 0, 1},
+                                 {7, 0, 0, 0, 2, 0, 0, 0, 6},
+                                 {0, 6, 0, 0, 0, 0, 2, 8, 0},
+                                 {0, 0, 0, 4, 1, 9, 0, 0, 5},
+                                 {0, 0, 0, 0, 8, 0, 0, 7, 9}};
+
+    cout << "Unsolved Sudoku : " << endl;
+    // pretty print sudoku unsolved
+    printSudoku(sudokuUnsolved2);
+    cout << endl;
+
+    // solve the unsolved sudoku
+    // if the solve sudoku return true, all empty number in sudoku have been filled
+    // if return false, sudoku dont have solution, because human error when creating sudoku unsolved
+    if (solveSudoku(sudokuUnsolved2) == true) {
+        cout << "Solved Sudoku : " << endl;
+        // pretty print sudoku solved
+        printSudoku(sudokuUnsolved2);
+        cout << endl;
+    } else {
+        cout << "No Solution For this Sudoku!" << endl;
+    }
+
     return 0;
 }
 
